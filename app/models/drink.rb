b036c007d7b8521ec655a2665362d2c69cbd3ad0
ioni90 <kkoioni27@gmail.com>
2020-12-31 13:22:19 +0900

    モデルのアソシエーション記述

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,3 +1,14 @@
 class Drink < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :type, :type2, :type3, :type4, :type5
   belongs_to :user
-end
+
+  with_options presence: true do
+    validates :start_time, uniqueness: true
+    validates :volume
+    validates :total_abv
+  end
+
+  validates :type_id, numericality: { other_than: 0 }
+
+ end
---
+++ test/models/type5_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Type5Test < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
