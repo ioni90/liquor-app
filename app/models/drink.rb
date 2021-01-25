6b555b506fdf5ae9a09fe1750b4908404f01aada
ioni90 <kkoioni27@gmail.com>
2021-01-25 21:23:19 +0900

    詳細表示画面の更新

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,6 +1,10 @@
 class Drink < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :type, :type2, :type3, :type4, :type5
+  belongs_to :type
+  belongs_to :type2
+  belongs_to :type3
+  belongs_to :type4
+  belongs_to :type5
   belongs_to :user
 
   with_options presence: true do
